8a618114fe37b153d2beded1dbdb9bb9dfd67cba
marcel <marcel@ubuntu.myguest.virtualbox.org>
2024-07-10 10:17:33 +0200

    Add model card, update train model

---
+++ model_card.md
@@ -0,0 +1,26 @@
+# Model Card
+
+For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
+
+## Model Details
+
+This model is a logistic regression classifier implemented using scikit-learn.
+
+## Intended Use
+
+This model is intended for binary classification where the goal is to predict the probability of a binary outcome based
+on the provided features.
+
+## Training Data
+
+Training data can be found at data/train.csv
+
+## Evaluation Data
+
+Evaluation data can be found at data/test.csv
+
+## Metrics
+
+Precision: 0.6962025316455697
+Recall: 0.24475524475524477
+Fbeta: 0.36218250235183447
--- train_model.py
+++ train_model.py
@@ -2,9 +2,10 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+import pickle
+import os
 from ml.data import process_data
-from ml.model import save_model_pickle, train_model, evaluate_slice
+from ml.model import save_model_pickle, train_model, evaluate_slice, inference, compute_model_metrics
 
 # Add code to load in the data.
 data = pd.read_csv("data/census.csv")
@@ -26,17 +27,29 @@ cat_features = [
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
+# Save encoder and lb
+with open(os.path.join('model', 'encoder.pkl'), 'wb') as encoder_file:
+    pickle.dump(encoder, encoder_file)
+with open(os.path.join('model', 'label_binarizer.pkl'), 'wb') as lb_file:
+    pickle.dump(lb, lb_file)
+train.to_csv("data/train.csv", index=False)
 
 # Proces the test data with the process_data function.
-# X_test, y_test, _, _ = process_data(
-#     test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
-# )
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+test.to_csv("data/test.csv", index=False)
 
 # Train and save a model.
 model = train_model(X_train, y_train)
 save_model_pickle(model=model)
 print("Model trained and saved successfully")
 
-# performance of the model on categorical slices of the data
+# Performance of the model on categorical slices of the data
 print("Evaluating model performance on categorical features:")
 evaluate_slice(model, test, cat_features, encoder, lb)
+
+# Performance in the test dataset
+print("Evaluating model performance on the testing data:")
+preds = inference(model, X_test)
+compute_model_metrics(y=y_test, preds=preds)
