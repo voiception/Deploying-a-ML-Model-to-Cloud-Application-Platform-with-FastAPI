f7ddf97a7265cefae8a18dc0f5ffae89e015ebaf
marcel <marcel@ubuntu.myguest.virtualbox.org>
2024-07-11 10:08:17 +0200

    update main.py for Render

--- main.py
+++ main.py
@@ -87,5 +87,6 @@ def predict(request: ModelInput) -> dict:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
 
